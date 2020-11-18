c95fa1dbc00960ab2b3e81404efe9d707c4efbd6
boriselec <boris.puzikov@gmail.com>
2020-11-18 17:09:26 +0300

    1.29

--- 1_3.rkt
+++ 1_3.rkt
@@ -1,10 +1,12 @@
+(define (inc x) (+ x 1))
 (define (sum term a next b)
   (if (> a b)
       0
       (+ (term a) (sum term (next a) next b))))
-(module learn-sicp sicp
-        (sum identity 1 inc 10))
+(sum
+  (lambda (x) x) 1 inc 10)
 
+; 1.29
 (define (integral f a b n)
   (define h (/ (- b a) n))
   (define (coeff k)
@@ -15,6 +17,8 @@
       (else 4)))
   (define (yk k)
     (f (+ a (* k h))))
-  )
-
-(integral 1 2 3 4)
+  (define (term k)
+    (* (coeff k) (yk k)))
+  (* (/ h 3) (sum term 0 inc n)))
+(integral (lambda (x) (* x x x)) 0 1 100)
+(integral sin 0 1 1000)
